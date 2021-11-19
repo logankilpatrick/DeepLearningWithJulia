abcc6ee0a872f35cfa8f0dbd16295e40fd0cce57
Logan Kilpatrick <23kilpatrick23@gmail.com>
2021-11-19 12:06:53 -0800

    Fix main file to show the right module

--- src/DeepLearningWithJulia.jl
+++ src/DeepLearningWithJulia.jl
@@ -1,35 +1,11 @@
-module BookTemplate
+module DeepLearningWithJulia
 
-using Reexport: @reexport
-@reexport begin
+# using Reexport: @reexport
+# @reexport begin
 
 using Books:
     build_all,
     gen
-
-using DataFrames:
-    DataFrame,
-    filter!,
-    filter,
-    select!,
-    select
-end # @reexport
-
-# include("data.jl")
-# export example_dataframe
-
-"""
-    build()
-
-This function is called during CI.
-"""
-function build()
-    println("Building Example Book")
-    # To avoid publishing broken websites.
-    fail_on_error = true
-    gen(; fail_on_error)
-    build_all(; fail_on_error)
-end
 
 include("ci.jl")
 
